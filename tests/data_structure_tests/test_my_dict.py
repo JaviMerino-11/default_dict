f163c1a79fe97e9de6dd6062b49650dff024bcfc
JaviMerino-11 <javier.merinochica.practicas@telefonica.com>
2022-05-24 09:42:58 +0200

    [[feat]] Now, if the key was not in the dictionary before, you append it with a default_value. So invalid key msg erased and also all the tests changed and implemented for the new version

--- tests/data_structure_tests/test_my_dict.py
+++ tests/data_structure_tests/test_my_dict.py
@@ -9,9 +9,9 @@ class TestDefaultDictRunner(unittest.TestCase):
     def test_default_dict_runner_key_OK(self):
         expected_key = 'some_key'
         expected_value = 'some_value'
-        expected_msg = 'Not found in current dictionary'
+        invalid_key_expected_msg = 'Not found in current dictionary'
 
-        test_dict = MyDict(default_missing_msg=expected_msg, default_value=11)
+        test_dict = MyDict(default_value_msg=invalid_key_expected_msg)
         test_dict[expected_key] = expected_value
 
         self.assertEqual(test_dict[expected_key], expected_value)
@@ -20,38 +20,38 @@ class TestDefaultDictRunner(unittest.TestCase):
         expected_key = 'some_key'
         expected_value = 'some_value'
         not_expected_key = 'some_invalid_key'
-        expected_msg = 'Not found in current dictionary'
+        default_value_msg = 'Default Value'
 
-        test_dict = MyDict(default_missing_msg=expected_msg, default_value=11)
+        test_dict = MyDict(default_value_msg=default_value_msg)
         test_dict[expected_key] = expected_value
 
-        self.assertEqual(test_dict[not_expected_key], expected_msg)
+        self.assertEqual(test_dict[not_expected_key], default_value_msg)
 
     def test_default_dict_runner_SAMUEL_1(self):
         expected_key = 'key_a'
         expected_value = 'value_a'
-        invalid_key_expected_msg = 'Not found in current dictionary'
+        default_value_msg = 'Default Value'
 
-        test_dict = MyDict(default_missing_msg=invalid_key_expected_msg, default_value=11)
-        test_dict2 = MyDict(default_missing_msg=invalid_key_expected_msg, default_value=11)
-        test_dict3 = MyDict(default_missing_msg=invalid_key_expected_msg, default_value=11)
+        test_dict = MyDict(default_value_msg=default_value_msg)
+        test_dict2 = MyDict(default_value_msg=default_value_msg)
+        test_dict3 = MyDict(default_value_msg=default_value_msg)
         test_dict2[expected_key] = expected_value
         test_dict3[expected_key] = expected_value
         del test_dict3[expected_key]
 
-        self.assertEqual(test_dict[expected_key], invalid_key_expected_msg)
+        self.assertEqual(test_dict[expected_key], default_value_msg)
         self.assertEqual(test_dict2[expected_key], expected_value)
-        self.assertEqual(test_dict3[expected_key], invalid_key_expected_msg)
+        self.assertEqual(test_dict3[expected_key], default_value_msg)
 
     def test_default_dict_runner_SAMUEL_LOOP(self):
         expected_key = 'key_a'
         unexpected_key = 'key_b'
         expected_value = 'value_a'
-        invalid_key_expected_msg = 'Not found in current dictionary'
+        default_value_msg = 'Default Value'
 
-        test_dict = MyDict(default_missing_msg=invalid_key_expected_msg, default_value=11)
+        test_dict = MyDict(default_value_msg=default_value_msg)
         test_dict[expected_key] = expected_value
-        test_dict2 = MyDict(default_missing_msg=invalid_key_expected_msg, default_value=11)
+        test_dict2 = MyDict(default_value_msg=default_value_msg)
         test_dict2.update(
             {
                 'key_a': 'value_a',
@@ -64,31 +64,31 @@ class TestDefaultDictRunner(unittest.TestCase):
             print(keys, '->', values)
 
         self.assertEqual(test_dict[expected_key], expected_value)
-        self.assertEqual(test_dict[unexpected_key], invalid_key_expected_msg)
+        self.assertEqual(test_dict[unexpected_key], default_value_msg)
         self.assertDictEqual(test_dict2, result_loop_dict)
 
     def test_default_dict_runner_SAMUEL_CONDITIONAL(self):
         expected_key = 'key_a'
         expected_value = 'value_a'
         unexpected_key = 'key_c'
-        invalid_key_expected_msg = 'Not found in current dictionary'
+        default_value_msg = 'Default Value'
 
-        test_dict = MyDict(default_missing_msg=invalid_key_expected_msg, default_value=11)
+        test_dict = MyDict(default_value_msg=default_value_msg)
         test_dict[expected_key] = expected_value
 
         if expected_key in test_dict:
             print(list(test_dict.keys()), '->', list(test_dict.values()))
         else:
-            print(invalid_key_expected_msg)
+            print(default_value_msg)
         self.assertTrue(expected_key in test_dict)
         self.assertFalse(unexpected_key in test_dict)
 
     def test_default_dict_runner_SAMUEL_(self):
         expected_key = 'key_a'
         unexpected_key = 'key_c'
-        invalid_key_expected_msg = 'Not found in current dictionary'
+        default_value_msg = 'Default Value'
 
-        d = MyDict(default_missing_msg=invalid_key_expected_msg, default_value=11)
+        d = MyDict(default_value_msg=default_value_msg)
         print(d[expected_key])
         self.assertTrue(expected_key in d)
         self.assertFalse(unexpected_key in d)
